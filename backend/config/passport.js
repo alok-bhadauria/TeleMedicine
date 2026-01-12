e35377a805a28b5d1752e56d2f03f4b3f25d45a6
Alok Bhadauria <alokbhadauria001@gmail.com>
2026-01-13 01:24:07 +0530

    Use relative callback URL for Google Strategy

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -28,7 +28,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
+        callbackURL: '/auth/google/callback'
     }, async (accessToken, refreshToken, profile, done) => {
         try {
             // Check if user exists
